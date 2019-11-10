fbdf962eddfd39b4770076087c4628c0ed8bb16e
Christina Gonzalez <cmgamway@gmail.com>
2019-11-10 21:41:37 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,10 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
 end
