7b882a8e22fdb9120f31995379ae8df35af33c03
Christina Gonzalez <cmgamway@gmail.com>
2019-11-11 03:33:06 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,6 @@
 class Owner
 
+  attr_accessor :pets
   attr_reader :species, :name
   @@all = []
 
@@ -7,6 +8,7 @@ class Owner
     @name = name
     @species = "human"
     @@all << self
+    @pets = {:dogs => [], :cats => []}
   end
 
   def say_species
@@ -23,5 +25,54 @@ class Owner
 
   def self.count
     @@all.size
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+    cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+    dog
+  end
+
+  def walk_dogs
+    Dog.all.collect do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    Cat.all.collect do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    Dog.all.collect do |dog|
+      dog.mood = "nervous"
+    end
+    Cat.all.collect do |cat|
+      cat.mood = "nervous"
+    end
+    Dog.all.collect do |dog|
+      dog.owner = nil
+    end
+    Cat.all.collect do |cat|
+      cat.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:dogs].count} dog(s) and #{pets[:cats].count} cat(s)."
   end
 end
