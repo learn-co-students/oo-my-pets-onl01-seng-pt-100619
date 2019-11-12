5e7d94eaf4ad5e36d5c677c88192e7962a9bdb82
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-12 18:21:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
-class Owner
-  # code goes here
+require 'pry'
+
+class Owner 
+  attr_reader :name, :species
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    # binding.pry
+  end
+  
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select{|cat| cat.owner = self}
+  end
+  
+  
 end
